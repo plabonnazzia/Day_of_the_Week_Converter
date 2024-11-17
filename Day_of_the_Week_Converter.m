c9965947bcbc68124d076057b63409426a5dd4b1
Nazia Sultana Plabon <72447776+plabonnazzia@users.noreply.github.com>
2024-11-17 13:48:13 +0600

    Add files via upload

---
+++ Day_of_the_Week_Converter.m
@@ -0,0 +1,19 @@
+dayNumber = input('Enter a number (1-7): ');
+switch dayNumber
+    case 1
+        disp('Sunday');
+    case 2
+        disp('Monday');
+    case 3
+        disp('Tuesday');
+    case 4
+        disp('Wednesday');
+    case 5
+        disp('Thursday');
+    case 6
+        disp('Friday');
+    case 7
+        disp('Saturday');
+    otherwise
+        disp('Invalid number. Enter a value between 1 and 7.');
+end
